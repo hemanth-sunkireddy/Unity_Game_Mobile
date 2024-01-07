24534a37feed982cb271bd8443cccb7b96de9cbd
hemanth-sunkireddy <hemanthkumarreddysunkireddy51@gmail.com>
2024-01-07 12:11:49 +0530

    Fixed score display in phone screen and Updated andriod app icon for mobiles

--- Assets/scripts/score.cs
+++ Assets/scripts/score.cs
@@ -6,10 +6,27 @@ using System.Collections;
 
 
 public class score: MonoBehaviour { 
+
+    public float timer = 0; 
+    
+    public float spawnRate = 3; 
+
     public TMP_Text _scoreText; 
     public int playerScore = 0; 
 
-    public void Update() { 
+
+    public void Update()
+    {
+        if ( timer < spawnRate ){
+            timer = timer + Time.deltaTime; 
+        }
+        else{
+            scoreUpdate();
+            timer = 0; 
+        }
+    }
+
+    public void scoreUpdate() { 
         _scoreText.text = "score: " + playerScore; 
         playerScore++;
     }
