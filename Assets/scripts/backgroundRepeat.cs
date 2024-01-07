9d2950f8e0aa497ce8cd1d0dabcc318ad91fddf0
hemanth-sunkireddy <hemanthkumarreddysunkireddy51@gmail.com>
2024-01-07 23:19:17 +0530

    refractor: Changed background spawn code, chore: removed unnecessary assets

--- Assets/scripts/backgroundRepeat.cs
+++ Assets/scripts/backgroundRepeat.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 
+
+
 public class backgroundRepeat : MonoBehaviour
 {
     public float scrollSpeed = 5f;
@@ -15,16 +17,5 @@ public class backgroundRepeat : MonoBehaviour
     {
         float moveAmount = Time.deltaTime * scrollSpeed;
         transform.Translate(Vector3.left * moveAmount);
-
-        if (transform.position.x < -backgroundWidth)
-        {
-            RepositionBackground();
-        }
-    }
-
-    public void RepositionBackground()
-    {
-        Vector3 newPosition = new Vector3(backgroundWidth * 2, 0, 0);
-        transform.position += newPosition;
     }
 }
--- Assets/scripts/score.cs
+++ Assets/scripts/score.cs
@@ -7,13 +7,13 @@ using System.Collections;
 
 public class score: MonoBehaviour { 
 
+    public int playerScore = 0; 
+
     public float timer = 0; 
     
     public float spawnRate = 3; 
 
     public TMP_Text _scoreText; 
-    public int playerScore = 0; 
-
 
     public void Update()
     {
