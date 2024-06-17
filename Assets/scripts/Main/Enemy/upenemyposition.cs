94df1ecea86aac1b53813ae62fc3dae6c6ebe203
hemanth-sunkireddy <hemanthkumarreddysunkireddy51@gmail.com>
2024-06-17 17:21:57 +0530

    updated start screen

--- Assets/scripts/Main/Enemy/upenemyposition.cs
+++ Assets/scripts/Main/Enemy/upenemyposition.cs
@@ -7,17 +7,14 @@ public class UpEnemyPosition : MonoBehaviour
     void Start()
     {
         up_enemy = GameObject.Find("UpEnemy");
-
+        
         if (up_enemy != null)
         {
             Camera mainCamera = Camera.main;
             Vector3 screenTopRight = new Vector3(Screen.width, Screen.height, mainCamera.nearClipPlane);
             Vector3 worldTopRight = mainCamera.ScreenToWorldPoint(screenTopRight);
-            worldTopRight.z = 0; // Assuming a 2D game or setting a proper z-axis value for your 3D game
-            
-            // Adjust position slightly down from the top-right corner
-            worldTopRight += new Vector3(-1, -1, 0); // Adjust x and y slightly down
-            
+            worldTopRight.z = 0; 
+            worldTopRight += new Vector3(-1, -1, 0); 
             up_enemy.transform.position = worldTopRight;
         }
         else
--- Assets/scripts/Start/start.cs
+++ Assets/scripts/Start/start.cs
@@ -2,12 +2,20 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class gameStart : MonoBehaviour
+public class start : MonoBehaviour
 {
     public Button StartButton;
     
     public void Start(){ 
         StartButton = GetComponent<Button>();
+        RectTransform rt = StartButton.GetComponent<RectTransform>();
+        rt.anchorMin = new Vector2(0, 0);
+        rt.anchorMax = new Vector2(0, 0);
+        rt.pivot = new Vector2(0, 0);
+        float screenWidth = Screen.width;
+
+        Debug.Log(screenWidth);
+        rt.anchoredPosition = new Vector2(screenWidth / 4, 50);
 
         if (StartButton != null)
         {
