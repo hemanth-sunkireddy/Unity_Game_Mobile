956580d4c89e666ec43ad5ab9448a8ee04174de3
hemanth-sunkireddy <hemanthkumarreddysunkireddy51@gmail.com>
2024-06-18 01:19:29 +0530

    added select level screen

--- Assets/scripts/Main/Enemy/First/Spawn1.cs
+++ Assets/scripts/Main/Enemy/First/Spawn1.cs
@@ -21,22 +21,14 @@ public class FirstEnemySpawn : MonoBehaviour
 
     public void enemyGenerate()
     {
-        // Get screen height in world units
         float screenHeight = Camera.main.orthographicSize * 2;
         float screenWidth = screenHeight * Camera.main.aspect;
-
-        // Calculate random Y position within the upper half of the screen height
         float minY = -(screenHeight/2) + 0.5f;
         float maxY = 0;
         float randomY = Random.Range(minY, maxY);
-
-        // Determine the X position (set to the right edge of the screen)
         float spawnX = screenWidth / 2;
-
-        // Convert screen coordinates to world coordinates
+        
         Vector3 spawnPosition = new Vector3(spawnX, randomY, 0);
-
-        // Create the enemy at the new random position
         GameObject newEnemy = Instantiate(firstenemy, spawnPosition, Quaternion.identity);
 
         Debug.Log("New enemy created at position: " + newEnemy.transform.position);
